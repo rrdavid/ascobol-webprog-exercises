0078aa85b9d210b8e736242a684f9eef9e8d3fd1
Ron David <davidroncarlo1313@gmail.com>
2017-03-24 10:57:28 +0800

    CodeIgniter2nd

--- Codeigniter/application/views/news/view.php
+++ Codeigniter/application/views/news/view.php
@@ -1,3 +1,3 @@
 <?php
-echo '<h2>'.$news_item['fullName'].'</h2>';
-echo '<strong>'.'Nickname: '.'</strong>'.$news_item['nickName'], '<br/>'.'<strong>'.'Email Address: '.'</strong>'.$news_item['email'], '<br/>'.'<strong>'.'Home Address: '.'</strong>'.$news_item['address'], '<br/>'.'<strong>'.'Gender: '.'</strong>'.$news_item['gender'], '<br/>'.'<strong>'.'Cellphone Number: '.'</strong>'.$news_item['cellphoneNumber'], '<br/>'.'<strong>'.'Comments: '.'</strong>'.'<br/>'.$news_item['comments'];
+echo '<h2>'.$news_item['Complete_Name'].'</h2>';
+echo '<strong>'.'Nickname: '.'</strong>'.$news_item['Nickname'], '<br/>'.'<strong>'.'Email Address: '.'</strong>'.$news_item['Email_Address'], '<br/>'.'<strong>'.'Home Address: '.'</strong>'.$news_item['Home_Address'], '<br/>'.'<strong>'.'Gender: '.'</strong>'.$news_item['Gender'], '<br/>'.'<strong>'.'Cellphone Number: '.'</strong>'.$news_item['Cell_phone_number'], '<br/>'.'<strong>'.'Comments: '.'</strong>'.'<br/>'.$news_item['Comments'];
